5dc0b57bbbddf45e38d262ca66637c28af48d1b6
Vincent Roduit <vincent.roduit@epfl.ch>
2024-04-11 17:11:45 +0200

    update tb

--- labs/lab4/verilog/ramDmaCi_tb.v
+++ labs/lab4/verilog/ramDmaCi_tb.v
@@ -10,24 +10,32 @@ module ramDmaCiTestbench;
     wire done;
     wire [31:0] result;
 
-    // Declare DUT as an instance of your ramDmaCi module
-    ramDmaCi DUT (
-        .start(start),
-        .clock(clock),
-        .reset(reset),
-        .valueA(valueA),
-        .valueB(valueB),
-        .ciN(ciN),
-        .done(done),
-        .result(result)
-    );
-
     initial begin
         reset = 1'b1;
         clock = 1'b0; // set the initial values
         repeat (4) #5 clock = ~clock; // generate 2 clock periods
         reset = 1'b0; // de-activate the reset
         forever #5 clock = ~clock; // generate a clock with a period of 10 time-units
+    end
+
+    ramDmaCi #(.customId(8'h0B)) DUT (
+        .start(start), 
+        .clock(clock), 
+        .reset(reset), 
+        .valueA(valueA), 
+        .valueB(valueB),
+        .ciN(ciN),
+        .done(), 
+        .result(result)
+    );
+
+    initial begin
+        valueA = 32'h00000000;
+        start = 1'b1;
+        @(negedge reset); /* wait for the reset period to end */
+        repeat(2) @(posedge clock); /* wait for 2 clock cycles */
+        #5
+        $finish;
     end
 
     initial begin
