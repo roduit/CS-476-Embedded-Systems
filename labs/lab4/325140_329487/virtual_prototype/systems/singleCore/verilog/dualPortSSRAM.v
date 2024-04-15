d4415f94020906bbddd21ecca181991d684ef1da
roduit <vincent.roduit@epfl.ch>
2024-04-15 14:44:40 +0200

    add files but need to check

--- labs/lab4/325140_329487/virtual_prototype/systems/singleCore/verilog/dualPortSSRAM.v
+++ labs/lab4/325140_329487/virtual_prototype/systems/singleCore/verilog/dualPortSSRAM.v
@@ -12,11 +12,14 @@ module dualPortSSRAM #( parameter bitwidth = 8,
 reg [bitwidth-1 : 0] memoryContent [nrOfEntries-1 : 0];
 
 always @(posedge clockA) begin
-if (readAfterWrite != 0) dataOutA <= memoryContent[addressA]; if (writeEnableA == 1'b1) memoryContent[addressA] <= dataInA; if (readAfterWrite == 0) dataOutA <= memoryContent[addressA];
+  if (readAfterWrite != 0) dataOutA = memoryContent[addressA]; 
+  if (writeEnableA == 1'b1) memoryContent[addressA] = dataInA; 
+  if (readAfterWrite == 0) dataOutA = memoryContent[addressA];
 end
-
-always @(posedge clockB) begin
-if (readAfterWrite != 0) dataOutB <= memoryContent[addressB]; if (writeEnableB == 1'b1) memoryContent[addressB] <= dataInB; if (readAfterWrite == 0) dataOutB <= memoryContent[addressB];
-end
+// always @(posedge clockB) begin
+//   if (readAfterWrite != 0) dataOutB = memoryContent[addressB]; 
+//   if (writeEnableB == 1'b1) memoryContent[addressB] = dataInB; 
+//   if (readAfterWrite == 0) dataOutB = memoryContent[addressB];
+//     end
     
 endmodule
