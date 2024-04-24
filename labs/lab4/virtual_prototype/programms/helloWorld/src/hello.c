f5d9f0582f652930c1018f5b389badf6a99c7149
Vincent Roduit <vincent.roduit@epfl.ch>
2024-04-24 12:44:52 +0200

    First try C code

--- labs/lab4/virtual_prototype/programms/helloWorld/src/hello.c
+++ labs/lab4/virtual_prototype/programms/helloWorld/src/hello.c
@@ -1,7 +1,21 @@
 #include <stdio.h>
+#include <ov7670.h>
+#include <swap.h>
 #include <vga.h>
 
 int main () {
   vga_clear();
-  printf("Hello World!\n" );
+  printf("Hello guys, welcome to my new channel !\n" );
+  volatile uint32_t memoryArray[16];
+
+  printf("Initialising memory array!\n" );
+  for (int i = 0; i < 16; i++) {
+    memoryArray[i] = i;
+  }
+  printf("Reading from the memory array!\n" );
+  for (int i = 0; i < 16; i++) {
+    printf("MemoryArray[%d] = %d\n", i, memoryArray[i]);
+  }
+
+  printf("Now test it with the DMA controller!\n" );
 }
