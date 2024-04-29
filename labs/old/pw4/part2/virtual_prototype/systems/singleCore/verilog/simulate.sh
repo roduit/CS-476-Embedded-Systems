0a42791d5dd76c0c11aa6abb02454ca450156cda
Vincent Roduit <vincent.roduit@epfl.ch>
2024-04-30 01:17:03 +0200

    add new version

---
+++ labs/old/pw4/part2/virtual_prototype/systems/singleCore/verilog/simulate.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+if [ -f "testbench" ]; then
+    rm testbench
+fi
+
+iverilog -s DMATestBench -o testbench BusTransaction_tb.v ramDmaCi.v DMAController.v dualPortSSRAM.v
+./testbench
+
+# iverilog -s DMATestBench -o testbench ramDmaCi_tb.v ramDmaCi.v DMAController.v dualPortSSRAM.v
+# ./testbench
+
+#gtkwave ramDmaCi.vcd &
