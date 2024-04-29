949c9d9341d101a46353440b620bf158a58aade1
roduit <vincent.roduit@epfl.ch>
2024-04-29 22:56:28 +0200

    PART2 WORKING

--- labs/pw4/part2/virtual_prototype/systems/singleCore/verilog/ramDmaCi.v
+++ labs/pw4/part2/virtual_prototype/systems/singleCore/verilog/ramDmaCi.v
@@ -87,7 +87,7 @@ module ramDmaCi #(  parameter [7:0]     customId = 8'h00)
     /// Done and result signal
     always @(posedge clock) begin
         start_reg <= start;
-        read_done <= reset ? 0 : start_reg ? enWR_CPU : 0;
+        read_done <= reset ? 0: start_reg ? enWR_CPU || enWR_DMA : 0;
         //read_done <= reset ? 0: enWR_CPU;
         
         busIn_grants_reg <= reset ? 0: busIn_grants;
@@ -99,7 +99,7 @@ module ramDmaCi #(  parameter [7:0]     customId = 8'h00)
         //DMA_memory_address_reg <= reset ? 0 : DMA_memory_address; 
     end
 
-    assign done     = reset ? 0 : (write ? 1'b1 : read_done);
+    assign done     = reset ? 0 : ((writeEnableA || write) ? 1'b1 : read_done) && s_isMyCi_no_start;
     assign result   = reset ? 0 : done ? (enWR_CPU ? resultSRAM_CPU : (enWR_DMA ? resultController : 32'b0)) : 32'b0;
     
     
