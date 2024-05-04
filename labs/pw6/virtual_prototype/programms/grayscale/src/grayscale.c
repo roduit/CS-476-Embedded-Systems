3cce1d821abb527e5e67194ca7794a3a254d6ee7
filippoquadri <filippo.quadri@epfl.ch>
2024-05-04 15:36:42 +0200

    lkm

--- labs/pw6/virtual_prototype/programms/grayscale/src/grayscale.c
+++ labs/pw6/virtual_prototype/programms/grayscale/src/grayscale.c
@@ -117,8 +117,8 @@ int main () {
         asm volatile("l.nios_rrr %[out1],%[in1],r0,20" :[out1]"=r"(pixel2):[in1] "r"(CIAddress+1));
 
 
-        asm volatile ("l.nios_rrr %[out1],%[in1],%[in2],0x9":[out1]"=r"(grayPixels):[in1]"r"(swap_u32(pixel1)),[in2]"r"(swap_u32(pixel2)));
-        gray[0] = grayPixels;
+        asm volatile ("l.nios_rrr %[out1],%[in1],%[in2],0x9":[out1]"=r"(grayPixels):[in1]"r"(pixel1),[in2]"r"(pixel2));
+        gray[0] = swap_u32(grayPixels);
         gray++;
       }
 
