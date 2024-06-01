ca6b9d54f4a78287c66fcda4e090b7a052efd223
roduit <vincent.roduit@epfl.ch>
2024-06-01 12:07:12 +0200

    spot mistake

--- project/virtual_prototype/programms/camera/src/camera.c
+++ project/virtual_prototype/programms/camera/src/camera.c
@@ -57,11 +57,40 @@ int main() {
     vga[2] = swap_u32(2);
     vga[3] = swap_u32((uint32_t)&newImageSobel[0]);
 
-    while (1) {
-        takeSingleImageBlocking((uint32_t)&grayscaleImage[0]);
-        compute_sobel_v1((uint32_t)&grayscaleImage[0], (uint8_t *)newImageSobel, camParams.nrOfPixelsPerLine, camParams.nrOfLinesPerImage, THRESHOLD);
-        // compare_arrays((uint8_t *)newImageSobel, (uint8_t *)oldImageSobel, (uint8_t *) grayscaleImage,  (uint16_t *)result, SIZE);
-        // memcpy((void*)oldImageSobel, (void*)newImageSobel, SIZE * sizeof(uint8_t));
+    // while (1) {
+    //     takeSingleImageBlocking((uint32_t)&grayscaleImage[0]);
+    //     compute_sobel_v1((uint32_t)&grayscaleImage[0], (uint8_t *)newImageSobel, camParams.nrOfPixelsPerLine, camParams.nrOfLinesPerImage, THRESHOLD);
+    //     // compare_arrays((uint8_t *)newImageSobel, (uint8_t *)oldImageSobel, (uint8_t *) grayscaleImage,  (uint16_t *)result, SIZE);
+    //     // memcpy((void*)oldImageSobel, (void*)newImageSobel, SIZE * sizeof(uint8_t));
+    // }
+
+    uint32_t tmplines[3][2];
+    uint32_t tmp_sobel_result = 0;
+    uint32_t threshold = THRESHOLD;
+
+    tmplines[0][0] = 0xFFFFFF0A;
+    tmplines[0][1] = 0x00000000;
+    tmplines[1][0] = 0xFFFFFF00;
+    tmplines[1][1] = 0x00000000;
+    tmplines[2][0] = 0x00000028;
+    tmplines[2][1] = 0x00000000;
+
+    uint32_t valueB = 0;
+    for (int nbLines = 0; nbLines < 3; nbLines++) {
+        printf("tmplines 0: %x\n", tmplines[nbLines][0]);
+        asm volatile ("l.nios_rrr r0,%[in1],%[in2],0xC"::[in1]"r"(tmplines[nbLines][0]),[in2]"r"((valueB)));
+        valueB++;
+
+        if (valueB == 5) {
+            valueB = 5 | (threshold << 8);
+        }
+        asm volatile ("l.nios_rrr %[out1],%[in1],%[in2],0xC":[out1]"=r"(tmp_sobel_result):[in1]"r"(tmplines[nbLines][1]),[in2]"r"((valueB)));
+        valueB++;
     }
+    // print the result in hex
+    printf("tmp_sobel_result conv 1: %x\n", tmp_sobel_result & 0xFF);
+    printf("tmp_sobel_result conv 2: %x\n", (tmp_sobel_result >> 8) & 0xFF);
+    printf("tmp_sobel_result conv 3: %x\n", (tmp_sobel_result >> 16) & 0xFF);
+    printf("tmp_sobel_result conv 4: %x\n", (tmp_sobel_result >> 24) & 0xFF);
     return 0;
 }
