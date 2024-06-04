9577fcf39fc3d36d45e5aaa57af3f43bf9bc5fa8
Vincent Roduit <vincent.roduit@epfl.ch>
2024-06-04 21:50:35 +0200

    update step 5

--- project/steps/5_pingpong_block/programms/streaming/build-release/support/src/exception.c.d
+++
@@ -1,3 +0,0 @@
-build-release/support/src/exception.c.o: support/src/exception.c \
- support/include/stdio.h support/include/printf.h support/include/defs.h \
- support/include/stdint.h
--- project/steps/5_pingpong_block/programms/streaming/support/src/assert.c
+++
@@ -1,9 +0,0 @@
-#include <assert.h>
-#include <stdio.h>
-
-int (*assert_printf)(const char*, ...) = &printf_;
-
-void assert_die() {
-    puts("dead!");
-    while (1);
-}
